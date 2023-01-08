06e990f8cc7569ff9aa91fd6342191ca4472bc93
James McNeil <mcneil_james@hotmail.com>
2023-01-08 14:34:25 +0000

    created basic user and todo models

---
+++ backend/models/todoModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const todoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  detail: {
+    type: String,
+    required: true,
+  },
+  comeplete: {
+    type: Boolean,
+    require: true,
+    default: false,
+  },
+})
---
+++ backend/models/user.Model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
