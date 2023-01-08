1aa932f95072eb9c7728efa8306e3d41e7ef3b13
James McNeil <mcneil_james@hotmail.com>
2023-01-08 14:19:06 +0000

    removed unused function

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -31,7 +31,6 @@ module.exports = {
   getTodos,
   getTodo,
   createTodo,
-  getSingleTodo,
   deleteTodo,
   updateTodo,
 }
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -4,7 +4,6 @@ const {
   getTodos,
   getTodo,
   createTodo,
-  getSingleTodo,
   deleteTodo,
   updateTodo,
 } = require('../controllers/todoController')
